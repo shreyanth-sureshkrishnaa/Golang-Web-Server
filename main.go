f7c8fc22bf11571bb43ef0edbe2099b5402d5b9c
shreyanth-sureshkrishnaa <shreyanthsureshkrishnaa.work@gmail.com>
2025-12-17 13:51:26 +0530

    All files added

---
+++ main.go
@@ -0,0 +1,57 @@
+// You always have to write package main.
+
+package main
+
+// Importing packages
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
+
+// Server Structure:
+// -> / (root) -> index.html
+// -> /hello -> hello func (Call a function, prints hello)
+// -> /form -> form func -> form.html
+
+func helloHandler(w http.ResponseWriter, r *http.Request) {
+	if r.URL.Path != "/hello" {
+		http.Error(w, "404 Not found", http.StatusNotFound)
+		return
+	}
+
+	if r.Method != "GET" {
+		http.Error(w, "Method is not supported.", http.StatusNotFound)
+		return
+	}
+
+	fmt.Fprintf(w, "Welcome to my static website!")
+}
+
+func formHandler(w http.ResponseWriter, r *http.Request) {
+	if err := r.ParseForm(); err != nil {
+		fmt.Fprintf(w, "ParseForm() err: %v", err)
+		return
+	}
+
+	fmt.Fprintf(w, "POST request successful ")
+	name := r.FormValue("name")
+	address := r.FormValue("address")
+	fmt.Fprintf(w, "Name = %s\n", name)
+	fmt.Fprintf(w, "Address = %s\n", address)
+
+}
+
+func main() {
+	fileServer := http.FileServer(http.Dir("./static")) //Dynamic Assignment
+	http.Handle("/", fileServer)
+	http.HandleFunc("/form", formHandler)
+	http.HandleFunc("/hello", helloHandler)
+
+	fmt.Printf("Starting server at port 8080\n")
+	if err := http.ListenAndServe(":8080", nil); err != nil {
+		log.Fatal(err)
+	}
+
+}
